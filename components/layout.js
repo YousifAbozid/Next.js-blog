0f708463b299f5fd48497cf1d4c9e6e3fad0fd4a
Yousif Abozid <yousif.abozid@yahoo.com>
2021-02-01 00:40:08 +0200

    create posts dynamic routes

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
 const name = "Yousif Abozid"
-export const siteTitle = "Next.js Sample Website"
+export const siteTitle = "Yousif's Blog"
 
 export default function Layout({ children, home }) {
     return (
